65b0cd01ee104ed74e6077b2f425f6092d42e567
Lukasz Krzywda <lukasz.krzywda@gmail.com>
2020-04-26 20:57:55 -0400

    new methods added

---
+++ src/main/java/com/zitt/ipreader/IpInfo.java
@@ -0,0 +1,22 @@
+package com.zitt.ipreader;
+
+/**
+ *
+ */
+public class IpInfo {
+
+    private String remoteAddr;
+
+    public IpInfo(String remoteAddr) {
+        this.remoteAddr = remoteAddr;
+    }
+
+    public String getRemoteAddr() {
+        return remoteAddr;
+    }
+
+    public void setRemoteAddr(String remoteAddr) {
+        this.remoteAddr = remoteAddr;
+    }
+
+}
---
+++ src/main/java/com/zitt/ipreader/IpReaderController.java
@@ -0,0 +1,36 @@
+package com.zitt.ipreader;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class IpReaderController {
+
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"};
+
+    @GetMapping("/ipinfo")
+    public IpInfo getIpInfo(HttpServletRequest request) {
+        for (String header : IP_HEADER_CANDIDATES) {
+            String ip = request.getHeader(header);
+            if (ip != null && ip.length() != 0
+                    && !"unknown".equalsIgnoreCase(ip)) {
+                return new IpInfo(ip);
+            }
+        }
+        return new IpInfo(request.getRemoteAddr());
+    }
+
+}
